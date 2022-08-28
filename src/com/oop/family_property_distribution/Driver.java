aa9fa026c554287dd04fe100c28fa2225e94a3bd
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2022-08-28 22:24:25 +0600

    Add files via upload

--- src/com/oop/family_property_distribution/Driver.java
+++ src/com/oop/family_property_distribution/Driver.java
@@ -19,24 +19,28 @@ public class Driver {
 			totChildren = in.nextInt();
 			
 			
-//			int intArray[] = new int[totChildren];
-//			String childName[] = new String[totChildren];
-//			int childAge[] = new int[totChildren];
-//			String childGender[] = new String[totChildren];
-//			 
-//			List<Children> listChildren = new ArrayList <Children>();
+			int intArray[] = new int[totChildren];
+			String childName[] = new String[totChildren];
+			int childAge[] = new int[totChildren];
+			String childGender[] = new String[totChildren];
+			 
+			List<Children> listChildren = new ArrayList <Children>();
 			
-//			for(int i = 0; i<intArray.length; i++) {
-//				System.out.println("Input the child name " + (i+1));
-//				childName[i] = in.next();
-//				System.out.println("Input the child age " + (i+1));
-//				childAge[i] = in.nextInt();
-//				System.out.println("Input the child gender " + (i+1));
-//				childGender[i] = in.next();
-//				Children children = new Children(childName[i], childAge[i], childGender[i]);
-//				listChildren.add(children);
-//				System.out.println(children.count);
-//			}
+			for(int i = 0; i<intArray.length; i++) {
+				System.out.println("Input the child name " + (i+1));
+				childName[i] = in.next();
+				System.out.println("Input the child age " + (i+1));
+				childAge[i] = in.nextInt();
+				System.out.println("Input the child gender " + (i+1));
+				childGender[i] = in.next();
+				
+			}
+			
+			for(int j=0;j<intArray.length;j++) {
+				Children children = new Children(childName[j], childAge[j], childGender[j]);
+				listChildren.add(children);
+			}
+			
 			
 			System.out.println();
 			System.out.println("Your father name is: " + fatherName);
@@ -44,27 +48,30 @@ public class Driver {
 			System.out.println("Your father have total " + totChildren + " childrens.");
 			System.out.println();
 			
-//			for(int k = 0; k<intArray.length; k++) {
-//				System.out.println();
-//				listChildren.get(k).displayChildren();
-//				if(childGender[k] == "Male") {
-//					System.out.format("Land get %.2f", 2 * listChildren.get(k).calLand(totLand));
-//				}else {
-//					System.out.format("%.2f",listChildren.get(k).calLand(totLand));
-//				}
-//				System.out.println();
-//			}
+			for(int k = 0; k<intArray.length; k++) {
+				System.out.println();
+				listChildren.get(k).displayChildren();
+				if(childGender[k] == "Male") {
+					System.out.format("Land get %.2f", 2 * listChildren.get(k).calLand(totLand));
+				}else {
+					System.out.format("%.2f",listChildren.get(k).calLand(totLand));
+				}
+				System.out.println();
+			}
 			
-			Children children1 = new Children("Nahid",32,"Male");
-			Children children2 = new Children("Munni",27,"Female");
-			Children children3 = new Children("Tinni",19,"Female");
 			
-			children1.displayChildren();
-			System.out.format("Land get %.2f", 2 * children1.calLand(totLand));
-			children2.displayChildren();
-			System.out.format("Land get %.2f", children2.calLand(totLand));
-			children3.displayChildren();
-			System.out.format("Land get %.2f", children3.calLand(totLand));
+			
+			// Below code is working fine
+//			Children children1 = new Children("Nahid",32,"Male");
+//			Children children2 = new Children("Munni",27,"Female");
+//			Children children3 = new Children("Tinni",19,"Female");
+//			
+//			children1.displayChildren();
+//			System.out.format("Land get %.2f", 2 * children1.calLand(totLand));
+//			children2.displayChildren();
+//			System.out.format("Land get %.2f", children2.calLand(totLand));
+//			children3.displayChildren();
+//			System.out.format("Land get %.2f", children3.calLand(totLand));
 			
 			}
 		

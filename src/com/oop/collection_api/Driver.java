95bef7549da9876201d2c4b5ddd8198212bcf834
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2022-09-12 22:15:15 +0600

    Add files via upload

--- src/com/oop/collection_api/Driver.java
+++ src/com/oop/collection_api/Driver.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -52,6 +53,27 @@ public class Driver {
         for(Object i:value_list){
             System.out.println(i);
         }
+        
+        /* 
+        LinkedList vs ArrayList
+        1. Both have Index
+        2. Both can have duplicate values
+        3. sorted by index
+        But when we fetch value 
+            - ArrayList is way faster then LinkedList
+        And when we Insert, Update and delete value
+            - LinkedList is way faster then ArrayList
+         */
+
+	    List<String> value_link_list = new LinkedList<>();
+	    value_link_list.add("Rumi");
+	    value_link_list.add("Ananta");
+	    value_link_list.add("Rumi");
+	    value_link_list.add(1, "Jeki");
+	
+	    for(String s:value_link_list){
+	        System.out.println(s);
+	    }
 
 
         Set<Integer> value_set = new HashSet<Integer>(); // cannot insert in between, always have unique values, not sorted

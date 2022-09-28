5dedf58e9623b3cedd984e8d518b2ab5ba8fdd4e
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2022-09-28 21:19:18 +0600

    Add files via upload

---
+++ src/com/proceduralvsoop/oop/OopApproach.java
@@ -0,0 +1,11 @@
+package com.oop.proceduralvsoop.oop;
+
+public class OopApproach {
+
+	public static void main(String[] args) {
+		Shape c = new Amoeba(3,5);
+        c.rotate();
+        c.playSound();
+	}
+
+}
